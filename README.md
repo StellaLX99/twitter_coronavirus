4118aee1d3a060261744bcc6ed0cc95bf50502df
Stella <stelli@cmc.edu>
2020-04-30 15:50:26 -0700

    finish

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Coronavirus analysis
 
-This repo analyzes the activity of coronavirus hashtags on twitter.
+This twitter analysis tracked the activity of corona hashtags. It gathered all geotagged tweets that have been sent since October 2019 when the coronavirus first began to spread till April.5th.2020. In total, there are roughly about 300 million tweets in this dataset.
+
+Since Twitter is a global social platform, it makes more sense to do the analysis to categorize those tweets into different languages and countries.
+
+Under this repo, under the viz folder, you would see two sub-folders, which is country and lang. The country folder shows the visual illustration of how many times a tweet has a certain hashtag(there are 17 hashtags in total) was sent out in a certain country. The lang folder does the same thing except recording the times of the tweet written in a certain language. Most of the tweets regarding coronavirus are tweeted in English and the majority of them were sent out in the U.S. The future analysis could look at certain time points in those tweets and see if the frequency corresponds with any local/national/global news happening at that time. 
+
+
--- src/map.py
+++ src/map.py
@@ -37,7 +37,7 @@ hashtags = [
 
 # initialize counters
 counter_lang = defaultdict(lambda: Counter())
-
+counter_country = defaultdict(lambda: Counter())
 # open the zipfile
 with zipfile.ZipFile(args.input_path) as archive:
 
@@ -53,16 +53,36 @@ with zipfile.ZipFile(args.input_path) as archive:
 
                 # load the tweet as a python dictionary
                 tweet = json.loads(line)
-
+                #print('tweet=',tweet)
+                #import pprint
+                #pprint.pprint(tweet)
+                #pprint.pprint(tweet["place"["country_code"])
+                #try:
+                    #print('tweet["place"["country_code"]=',twitter["place"["country_code"])
+                #except TypeError:
+                    #pass
+                    #pprint.pprint(tweet)
+                    #crash_python
+             #print('tweet["country_code"]=', tweet["country_code"] 
+                #print('tweet["place"]["country_code"]=',tweet["place"]["country_code"]
+                #crash_python
+                
                 # convert text to lower case
                 text = tweet['text'].lower()
 
                 # search hashtags
                 for hashtag in hashtags:
                     lang = tweet['lang']
+                    if tweet['place'] != None and tweet['place']['country_code'] != None:
+                        country = tweet['place']['country_code']
+                    else:
+                        country = 'und'
                     if hashtag in text:
                         counter_lang[hashtag][lang] += 1
+                        counter_country[hashtag][country] +=1
                     counter_lang['_all'][lang] += 1
+                    counter_country['_all'][country] += 1
+
 
 # open the outputfile
 try:
@@ -72,7 +92,9 @@ except FileExistsError:
 output_path_base = os.path.join(args.output_folder,os.path.basename(args.input_path))
 
 output_path_lang = output_path_base+'.lang'
-print('saving',output_path_lang)
+output_path_country = output_path_base+'.country'
+print('saving',output_path_lang,output_path_country)
 with open(output_path_lang,'w') as f:
     f.write(json.dumps(counter_lang))
-
+with open(output_path_country,'w') as f:
+    f.write(json.dumps(counter_country))
